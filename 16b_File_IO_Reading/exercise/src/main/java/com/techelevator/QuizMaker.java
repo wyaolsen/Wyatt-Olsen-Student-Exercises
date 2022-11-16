3e888a813c320b9826482e852b117049a487f035
Wyatt <wolsen23@gmail.com>
2022-11-16 02:05:49 -0700

    File IO reading first commit

--- 16b_File_IO_Reading/exercise/src/main/java/com/techelevator/QuizMaker.java
+++ 16b_File_IO_Reading/exercise/src/main/java/com/techelevator/QuizMaker.java
@@ -1,9 +1,66 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class QuizMaker {
 
 	public static void main(String[] args) {
-		
+		Scanner userInput = new Scanner(System.in);
+
+		System.out.println("Enter the fully qualified name of the file to read in for quiz questions");
+		String filename = userInput.nextLine();
+
+		startQuiz(filename);
+
+	}
+
+	public static void startQuiz(String filename){
+		Scanner userinput = new Scanner(System.in);
+
+		try{
+			File quizFile = new File(filename);
+			Scanner fileScanner = new Scanner(quizFile);
+
+			int questionCount = 0;
+			int correctAnswerCount = 0;
+
+			while(fileScanner.hasNextLine()){
+				String[] question = fileScanner.nextLine().split("\\|");
+
+				//print question
+				System.out.println(question[0]);
+				int correctAnswer = 0;
+				//print answers
+				for(int i = 1; i < question.length; i++ ){
+					if(question[i].contains("*")){
+
+						correctAnswer = i;
+					}
+					System.out.println(i + ". " + question[i].replace("*", ""));
+				}
+
+				System.out.println();
+				System.out.print("Your answer: ");
+				int userAnswer = userinput.nextInt();
+				if(correctAnswer == userAnswer){
+					System.out.println("RIGHT!");
+					correctAnswerCount++;
+				}else{
+					System.out.println("WRONG!");
+				}
+				System.out.println();
+
+				questionCount++;
+			}
+
+			System.out.println("You got " + correctAnswerCount + " answer(s) correct out of the " + questionCount + " questions asked.");
+
+			fileScanner.close();
+		}catch(FileNotFoundException e){
+			System.out.println(e);
+		}
 
 	}
 
--- 16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
+++ 16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
@@ -1,9 +1,63 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class WordSearch {
 
 	public static void main(String[] args) {
+		Scanner userInput = new Scanner(System.in);
+
+		//file name user input
+		System.out.println("What is the fully qualified name of the file that should be searched?");
+		String fileName = userInput.nextLine();
+
+		System.out.println("What is the search word you are looking for?");
+		String searchWord = userInput.nextLine();
+
+		System.out.println("Should the search be case sensitive? (Y\\N)");
+		String isSensitive = userInput.nextLine();
+
+
+
+		searchFile(fileName,searchWord,isSensitive);
+
+
 
 	}
+
+
+
+	public static void searchFile(String fileName, String searchWord, String inSensitive){
+		try{
+			File wordSearchFile = new File(fileName);
+			Scanner fileScanner = new Scanner(wordSearchFile);
+
+			int lineCount = 0;
+
+			while(fileScanner.hasNextLine()){
+				lineCount++;
+				String fileNextLine = fileScanner.nextLine();
+
+				// case insensitive
+				if(inSensitive.equals("Y")){
+					if(fileNextLine.contains(searchWord)){
+						System.out.println(lineCount + ") " + fileNextLine);
+					}
+				}else if(inSensitive.equals("N")){
+					if(fileNextLine.toLowerCase().contains(searchWord.toLowerCase())){
+						System.out.println(lineCount + ") " + fileNextLine);
+					}
+				}
+
+
+			}
+			fileScanner.close();
+		}catch (FileNotFoundException e){
+			System.out.println(e);
+		}
+	}
+
 
 }
